4adfe6aa49bad3e01344d659a4cb87ed82c469ba
twist-tracer <twist.tracer@gmail.com>
2022-04-14 17:29:08 +0300

    <patterns> strategy

---
+++ app/Architecture/Patterns/Behavioral/Strategy/ConcreteStrategyB.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Architecture\Patterns\Behavioral\Strategy;
+
+class ConcreteStrategyB implements Strategy
+{
+    public function doAlgorithm(array $data): array
+    {
+        rsort($data);
+
+        return $data;
+    }
+}
