013365cbea2e65801c60a93331625b742cd00a56
twist-tracer <twist.tracer@gmail.com>
2022-04-21 09:12:23 +0300

    <patterns> chain of responsibility

---
+++ app/Patterns/Behavioral/ChainOfResponsibility/AbstractHandler.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Patterns\Behavioral\ChainOfResponsibility;
+
+/**
+ * Поведение цепочки по умолчанию может быть реализовано внутри базового класса
+ * обработчика.
+ */
+abstract class AbstractHandler implements Handler
+{
+    /**
+     * @var Handler
+     */
+    private $nextHandler;
+
+    public function setNext(Handler $handler): Handler
+    {
+        $this->nextHandler = $handler;
+        // Возврат обработчика отсюда позволит связать обработчики простым
+        // способом, вот так:
+        // $monkey->setNext($squirrel)->setNext($dog);
+        return $handler;
+    }
+
+    public function handle(string $request): ?string
+    {
+        if ($this->nextHandler) {
+            return $this->nextHandler->handle($request);
+        }
+
+        return null;
+    }
+}
---
+++ app/Patterns/Behavioral/ChainOfResponsibility/MonkeyHandler.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Patterns\Behavioral\ChainOfResponsibility;
+
+/**
+ * Все Конкретные Обработчики либо обрабатывают запрос, либо передают его
+ * следующему обработчику в цепочке.
+ */
+class MonkeyHandler extends AbstractHandler
+{
+    public function handle(string $request): ?string
+    {
+        if ($request === "Banana") {
+            return "Monkey: I'll eat the " . $request . ".\n";
+        } else {
+            return parent::handle($request);
+        }
+    }
+}
