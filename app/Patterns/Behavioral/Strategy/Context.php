6906e410a9b3b4d139d1d7b6e886bf2bf024f5b4
twist-tracer <twist.tracer@gmail.com>
2022-04-21 11:07:12 +0300

    <patterns> updated strategy

--- app/Patterns/Behavioral/Strategy/Context.php
+++ app/Patterns/Behavioral/Strategy/Context.php
@@ -8,31 +8,27 @@ namespace App\Patterns\Behavioral\Strategy;
 class Context
 {
     /**
-     * @var Strategy Контекст хранит ссылку на один из объектов Стратегии.
+     * Обычно Контекст принимает стратегию через конструктор, а также
+     * предоставляет сеттер для её изменения во время выполнения.
+     *
+     * @param Strategy $strategy Контекст хранит ссылку на один из объектов Стратегии.
      * Контекст не знает конкретного класса стратегии. Он должен работать со
      * всеми стратегиями через интерфейс Стратегии.
      */
-    private $strategy;
-
-    /**
-     * Обычно Контекст принимает стратегию через конструктор, а также
-     * предоставляет сеттер для её изменения во время выполнения.
-     */
-    public function __construct(Strategy $strategy)
+    public function __construct(private Strategy $strategy)
     {
-        $this->strategy = $strategy;
     }
 
     /**
      * Обычно Контекст позволяет заменить объект Стратегии во время выполнения.
      */
-    public function setStrategy(Strategy $strategy)
+    public function setStrategy(Strategy $strategy): void
     {
         $this->strategy = $strategy;
     }
 
     /**
-     * Вместо того, чтобы самостоятельно реализовывать множественные версии
+     * Вместо того чтобы самостоятельно реализовывать множественные версии
      * алгоритма, Контекст делегирует некоторую работу объекту Стратегии.
      */
     public function doSomeBusinessLogic(): string
@@ -41,8 +37,6 @@ class Context
 
 //        echo "Context: Sorting data using the strategy (not sure how it'll do it)\n";
         $result = $this->strategy->doAlgorithm(["a", "b", "c", "d", "e"]);
-        return implode(",", $result) . "\n";
-
-        // ...
+        return implode(",", $result);
     }
 }
